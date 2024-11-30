6e6cbd8d965c62e5f075b9277f48c0e67894d0ba
Zaid Syed <zaidsyed@icloud.com>
2024-11-30 15:25:58 -0500

    initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import TabBar from './components/TabBar';
 import PageTransition from './components/PageTransition';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const plusJakarta = Plus_Jakarta_Sans({ 
   subsets: ['latin'],
@@ -33,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={plusJakarta.variable}>
+      <body className="font-sans">
         <PageTransition>
           {children}
         </PageTransition>
