10909c374808757349644d89a32022377d376e4f
Zaid Syed <zaidsyed@icloud.com>
2024-12-05 11:00:22 -0500

    initial

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -41,7 +41,7 @@ export default function ProfilePage() {
     };
 
     return (
-        <div className="max-w-screen-2xl mx-auto">
+        <div className="mx-auto">
             <div className="flex items-center justify-center my-5">
                 <img src="https://launchgood.s3.amazonaws.com/static/logos/launchgood-black.svg"
                     className="h-4 dark:invert"
